170e7cc1a6f1a94fb7b6ca1ba8e27018773f5ff6
Philip Ng <phil__n@hotmail.com>
2020-04-02 04:41:28 -0400

    added api routes and password for db config file; server working properly

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,17 +1,23 @@
+// Requiring path to so we can use relative routes to our HTML files
+var path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
-    // if (req.user) {
-    //   res.redirect("/members");
-    // }
+    if (req.user) {
+      res.redirect("/members");
+    }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
-    // if (req.user) {
-    //   res.redirect("/members");
-    // }
+    if (req.user) {
+      res.redirect("/members");
+    }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
--- server.js
+++ server.js
@@ -13,14 +13,9 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({
-    secret: "pineapples on pizza",
-    resave: true,
-    saveUninitialized: true
-  })
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -31,11 +26,7 @@ require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+  app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
   });
 });
