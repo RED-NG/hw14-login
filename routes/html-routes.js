e2560c887164048a57c4e7a64dfb50fc81f0f207
Philip Ng <phil__n@hotmail.com>
2020-04-02 03:59:52 -0400

    added passport config files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,17 +1,17 @@
 module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
+    // if (req.user) {
+    //   res.redirect("/members");
+    // }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
+    // if (req.user) {
+    //   res.redirect("/members");
+    // }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
