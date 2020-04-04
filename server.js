b20950bbf34037c3ac997ea45f6f7c6bed902867
Philip Ng <phil__n@hotmail.com>
2020-04-04 13:35:19 -0400

    authentication working properly with mysql; to be upscaled to jawsdb after deploying to heroku

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
+db.sequelize.sync().then(function () {
   app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
   });
